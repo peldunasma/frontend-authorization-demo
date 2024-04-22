206aa06c92f684f007d3d8abc8a41ddd987ec00e
Matthew Peldunas <peldunasma@gmail.com>
2024-04-22 18:36:04 -0400

    signout function not working

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,24 +1,21 @@
+import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 
-export default function ProtectedRoute({
-  isLoggedIn,
-  children,
-  anonymous = false,
-}) {
+import AppContext from "../context/AppContext";
+
+export default function ProtectedRoute({ children, anonymous = false }) {
   const location = useLocation();
   const from = location.state?.from || "/";
-  // If you can visit without authorization, but you are already authorized
+
+  const { isLoggedIn } = useContext(AppContext);
+
   if (anonymous && isLoggedIn) {
-    // ...then go back the the path you came from
     return <Navigate to={from} />;
   }
 
-  // If you need to be authorize to visit this path but you aren't logged in yet
   if (!anonymous && !isLoggedIn) {
-    // ...then go to login page
     return <Navigate to="/login" state={{ from: location }} />;
   }
 
-  // if ok show the content
   return children;
 }
---
+++ src/context/AppContext.js
@@ -0,0 +1,7 @@
+// AppContext.jsx
+
+import React from "react";
+
+const AppContext = React.createContext();
+
+export default AppContext;
